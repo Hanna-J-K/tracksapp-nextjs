c9cfae3b11cd2217433118663d621f8ee24fe205
Hanna Kraska <hanna.kraska11@gmail.com>
2023-06-13 18:33:36 +0200

    wrap with chakra provider

---
+++ src/app/components/playerLayout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Box } from "@chakra-ui/layout";
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        sidebar
+      </Box>
+      <Box marginLeft="250px">{children}</Box>
+      <Box position="absolute" bottom="0" left="0">
+        player
+      </Box>
+    </Box>
+  );
+};
+
+export default PlayerLayout;
